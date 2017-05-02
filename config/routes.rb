167b6608296dc8bd5ee8fe10fa845bf7355f3dcf
bliareg <bliareg3@gmail.com>
2017-05-02 15:40:20 +0300

    Add task resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   apipie
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/' => 'tasks#index'
   namespace :api do
     namespace :v1 do
       resources :users, except: :update
